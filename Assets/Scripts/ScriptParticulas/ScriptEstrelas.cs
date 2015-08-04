40f25022f137e0d169f68923f5e432d2bb439a70
Valdir de Souza Junior <jr140797@gmail.com>
2015-08-04 10:28:42 -0300

    Particulas Morrem dps de um tempo

--- Assets/Scripts/ScriptParticulas/ScriptEstrelas.cs
+++ Assets/Scripts/ScriptParticulas/ScriptEstrelas.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class ScriptEstrelas : MonoBehaviour {
 
-	// Use this for initialization
+	public float tempopassado;
+	void Awake(){
+		tempopassado = 0;
+	}
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		tempopassado += Time.deltaTime;
+		if (tempopassado > 4) {
+			Destroy (gameObject); 
+		}
+
 	}
 }
