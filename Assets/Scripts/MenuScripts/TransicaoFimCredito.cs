ed483cf385092e35a406f22b19a28f906053e354
bratus <alajoan.jonathan@gmail.com>
2015-08-07 18:03:52 -0300

    Updateado

---
+++ Assets/Scripts/MenuScripts/TransicaoFimCredito.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransicaoFimCredito : MonoBehaviour {
+	public GameObject canvasFim;
+	public GameObject canvasCredito;
+
+	public void passar (){
+		canvasFim.SetActive (false);
+		canvasCredito.SetActive (true);
+	}
+}
